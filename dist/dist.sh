eac8a54904fd2ce11e4cd74ee30e0d972c84ea01
ixtab <ixtab@fs-xtc.net>
2012-09-05 02:37:07 +0200

    Fixed kindletool usage

--- dist/dist.sh
+++ dist/dist.sh
@@ -3,9 +3,9 @@ VERSION=2.3.1
 
 rm *.bin *.zip
 
-# This requires NiLuJe's kindletool
-kindletool create ota2 -d k5w -d k5g -d k5gb -d k5u src/install update_${PRODUCT}_${VERSION}_install.bin
-kindletool create ota2 -d k5w -d k5g -d k5gb -d k5u src/uninstall update_${PRODUCT}_${VERSION}_uninstall.bin
+# This requires NiLuJe's kindletool, version >= 1.5.0
+kindletool create ota2 -d k5w -d k5g -d k5gb -d k5u -C src/install update_${PRODUCT}_${VERSION}_install.bin
+kindletool create ota2 -d k5w -d k5g -d k5gb -d k5u -C src/uninstall update_${PRODUCT}_${VERSION}_uninstall.bin
 
 zip -9 -r ${PRODUCT}_${VERSION}.zip update_${PRODUCT}_${VERSION}_install.bin update_${PRODUCT}_${VERSION}_uninstall.bin *.txt src/
 
