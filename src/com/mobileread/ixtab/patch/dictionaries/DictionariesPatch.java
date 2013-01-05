c73b6413588b57cd1db6530060a77b996a39204b
ixtab <ixtab@fs-xtc.net>
2013-01-05 16:26:40 +0100

    latest version of dictionaries patch, small bugfix + cleanup

--- src/com/mobileread/ixtab/patch/dictionaries/DictionariesPatch.java
+++ src/com/mobileread/ixtab/patch/dictionaries/DictionariesPatch.java
@@ -13,12 +13,34 @@ import com.mobileread.ixtab.jbpatch.PatchMetadata.PatchableClass;
 
 public class DictionariesPatch extends Patch {
 
-	private static final String CLASS = "com.amazon.ebook.booklet.reader.plugin.systemcards.DictionaryCard";
-	public static final String MD5_BEFORE = "cc8be7129f5b45d370f5b3fd41cd069f";
-	private static final String MD5_AFTER = "b81427199453be5c3077e81b1c3f893b";
+	private static final String CLASS_510 = "com.amazon.ebook.booklet.reader.plugin.systemcards.DictionaryCard";
+	public static final String MD5_BEFORE_510 = "cc8be7129f5b45d370f5b3fd41cd069f";
+	private static final String MD5_AFTER_510 = "b81427199453be5c3077e81b1c3f893b";
 
+	private static final String CLASS_531 = "com.amazon.ebook.booklet.reader.plugin.systemcards.G";
+	public static final String MD5_BEFORE_531 = "f19fe224aae57e9a972dd439127418bd";
+	private static final String MD5_AFTER_531 = "7dab5acc5467969add653deb3e7824ce";
+
+	private static Boolean isPaperwhite;
+	
 	public int getVersion() {
-		return 20120913;
+		return 20130105;
+	}
+	
+	static boolean isPaperWhite() {
+		if (isPaperwhite == null) {
+			synchronized (DictionariesPatch.class) {
+				if (isPaperwhite == null) {
+					try {
+						Class.forName("com.amazon.ebook.util.lang.UUID");
+						isPaperwhite = Boolean.FALSE;
+					} catch (Throwable t) {
+						isPaperwhite = Boolean.TRUE;
+					}
+				}
+			}
+		}
+		return isPaperwhite.booleanValue();
 	}
 
 	public Permission[] getRequiredPermissions() {
@@ -33,36 +55,54 @@ public class DictionariesPatch extends Patch {
 	}
 
 	public PatchMetadata getMetadata() {
-		PatchableClass pc = new PatchableClass(CLASS).withChecksums(MD5_BEFORE,
-				MD5_AFTER);
-		return new PatchMetadata(this).withClass(pc);
+		PatchMetadata md = new PatchMetadata(this);
+		if (isPaperWhite()) {
+			md.withClass(new PatchableClass(CLASS_531).withChecksums(MD5_BEFORE_531,
+					MD5_AFTER_531));
+		} else {
+			md.withClass(new PatchableClass(CLASS_510).withChecksums(MD5_BEFORE_510,
+					MD5_AFTER_510));
+		}
+		return md;
 	}
 
 	public String perform(String md5, BCClass clazz) throws Throwable {
-		if (md5.equals(MD5_BEFORE)) {
-			patchMethodK(clazz);
+		if (md5.equals(MD5_BEFORE_510)) {
+			patchMethodK510(clazz);
+			return null;
+		} else if (md5.equals(MD5_BEFORE_531)) {
+			patchMethod_cO_531(clazz);
 			return null;
 		}
 		return "unsupported MD5: "+md5;
 	}
 
-	private void patchMethodK(BCClass clazz) throws Exception {
+	private void patchMethodK510(BCClass clazz) throws Exception {
 		Code c = clazz.getDeclaredMethod("K", new Class[] {int.class}).getCode(false);
 		c.before(192);
 		c.dup();
 		c.iload().setLocal(1);
-		c.invokestatic().setMethod(DictionariesPatch.class.getDeclaredMethod("onMethodK", new Class[]{Object.class, int.class}));
+		c.invokestatic().setMethod(DictionariesPatch.class.getDeclaredMethod("onGetCard", new Class[]{Object.class, int.class}));
 		c.calculateMaxLocals();
 		c.calculateMaxStack();
 		
 	}
 	
-	public static void onMethodK(Object o, int currentIndex) {
-		if (o == null) {
-			// should never happen
-			return;
+	private void patchMethod_cO_531(BCClass clazz) throws Exception {
+		Code c = clazz.getDeclaredMethod("cO", new Class[] {int.class}).getCode(false);
+		c.before(189);
+		c.dup();
+		c.iload().setLocal(1);
+		c.invokestatic().setMethod(DictionariesPatch.class.getDeclaredMethod("onGetCard", new Class[]{Object.class, int.class}));
+		c.calculateMaxLocals();
+		c.calculateMaxStack();
+		
+	}
+	
+	public static void onGetCard(Object o, int currentIndex) {
+		if (o != null) {
+			Backend.INSTANCE.modifyPanel(o, currentIndex);
 		}
-		Backend.modifyPanel(o, currentIndex);
 	}
 	
 }
---
+++ src/com/mobileread/ixtab/patch/dictionaries/fw531/Backend531.java
@@ -0,0 +1,170 @@
+package com.mobileread.ixtab.patch.dictionaries.fw531;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import com.amazon.agui.swing.KDialog;
+import com.amazon.ebook.booklet.reader.plugin.systemcards.G;
+import com.amazon.ebook.booklet.reader.sdk.ui.e;
+import com.mobileread.ixtab.patch.dictionaries.Backend;
+
+// This class only exists because if the patch itself refers to any of its patched classes,
+// things are likely to screw up.
+
+public class Backend531 implements ActionListener, PropertyChangeListener, Backend {
+	
+	private static final JButton previousButton = new JButton(" <<   ");
+	private static final JButton nextButton = new JButton("   >> ");
+	private static G dictCard = null;
+	private static int dictIndex = 0;
+	private static JButton definitionButton = null;
+	private static Backend531 INSTANCE;
+	
+	public Backend531() {
+		INSTANCE = this;
+		previousButton.addActionListener(INSTANCE);
+		nextButton.addActionListener(INSTANCE);
+	}
+	
+	public void modifyPanel(Object o, int currentIndex) {
+		dictIndex = currentIndex;
+		dictCard = (G) o;
+		Component[] children = dictCard.getComponents();
+		for (int i=0; i < children.length; ++i) {
+			if (children[i] instanceof e) {
+				modifyLinkPanel((e)children[i]);
+				break;
+			}
+		}
+		previousButton.setEnabled(getNextUsefulDictionaryIndex(dictIndex, -1) != null);
+		nextButton.setEnabled(getNextUsefulDictionaryIndex(dictIndex, 1) != null);
+	}
+	
+	private static void modifyLinkPanel(e linkPanel) {
+		Component children[] = linkPanel.getComponents();
+		JButton theButton = null;
+		JPanel thePanel = null;
+		for (int i=0; i < children.length; ++i) {
+			if (children[i] instanceof JPanel) {
+				JPanel childPanel = (JPanel) children[i];
+				Component[] childComponents = childPanel.getComponents();
+				for (int ci=0; ci < childComponents.length; ++ci) {
+					if (childComponents[ci] instanceof ButtonsPanel) {
+						return;
+					}
+					if (childComponents[ci] instanceof JButton) {
+						theButton = (JButton) childComponents[ci];
+						thePanel = childPanel;
+						break;
+					}
+				}
+			}
+		}
+		if (theButton != null && thePanel != null) {
+			if (theButton != definitionButton) {
+				if (definitionButton != null) {
+					definitionButton.removePropertyChangeListener(INSTANCE);
+				}
+				else {
+					previousButton.setBorder(theButton.getBorder());
+					nextButton.setBorder(theButton.getBorder());
+					previousButton.setFont(theButton.getFont());
+					nextButton.setFont(theButton.getFont());
+				}
+				definitionButton = theButton;
+				definitionButton.addPropertyChangeListener(INSTANCE);
+			}
+			thePanel.remove(theButton);
+			JPanel buttons = new ButtonsPanel(new BorderLayout(10, 10));
+			buttons.add(previousButton, BorderLayout.WEST);
+			buttons.add(theButton, BorderLayout.CENTER);
+			buttons.add(nextButton, BorderLayout.EAST);
+			thePanel.add(buttons, BorderLayout.SOUTH);
+		}
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		final Object src = e.getSource();
+		new Thread() {
+			public void run() {
+				if (src instanceof JButton) {
+					JButton button = (JButton) src;
+					if (button == previousButton) {
+						Integer newPosition = getNextUsefulDictionaryIndex(dictIndex, -1);
+						if (newPosition != null) {
+							gotoDict(newPosition);
+						}
+					} else if (button == nextButton) {
+						Integer newPosition = getNextUsefulDictionaryIndex(dictIndex, 1);
+						if (newPosition != null) {
+							gotoDict(newPosition);
+						}
+					}
+				}
+			}
+		}.start();
+	}
+	
+	public void propertyChange(PropertyChangeEvent evt) {
+		if ("enabled".equals(evt.getPropertyName())) {
+			boolean enabled = ((Boolean) evt.getNewValue()).booleanValue();
+			previousButton.setEnabled(enabled);
+			nextButton.setEnabled(enabled);
+		}
+	}
+
+
+	private void gotoDict(final Integer newPosition) {
+		if (newPosition != null) {
+			SwingUtilities.invokeLater(new Runnable() {
+
+				public void run() {
+					dictCard.cO(newPosition.intValue());
+					Component dialog = dictCard;
+					while (!(dialog instanceof KDialog)) {
+						dialog = dialog.getParent();
+						if (dialog == null) {
+							// should never happen
+							dialog = dictCard;
+							break;
+						}
+					}
+					if (dialog instanceof KDialog) {
+						((KDialog)dialog).setTitle(dictCard.iO(newPosition.intValue()));
+					}
+					dialog.validate();
+					dialog.repaint();
+				}
+			});
+		}
+	}
+	
+	private static Integer getNextUsefulDictionaryIndex(int current, int delta) {
+		// delta is assumed to be either +1, or -1.
+		int total = dictCard.ON();
+		for (int i= current + delta; i >= 0 && i < total; i += delta) {
+			if (dictCard.jM(i)) {
+				return new Integer(i);
+			}
+		}
+		return null;
+	}
+	
+	private static class ButtonsPanel extends JPanel {
+		private static final long serialVersionUID = 1L;
+		
+		public ButtonsPanel(LayoutManager layout) {
+			super(layout);
+		}
+	}
+
+}
